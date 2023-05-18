6273337163c09a00855f75a7e4d966085a0e1158
Colton Jackson <coltonajackson1@gmail.com>
2023-05-17 23:16:27 -0400

    Updated 'guess_game_simple.py'

--- py/guess_game_simple.py
+++ py/guess_game_simple.py
@@ -13,31 +13,35 @@ Congratulations! You guessed it!
 It took you 3 guesses.
 """
 
+import random
+
+random_words = ["phone", "flowers", "towel", "laptop", "mother", "sealing", "book"]
+
 print("Welcome to the word guessing game!\n")
 
-answer = input("What is the word? ")
-#hint_split = ["_" for c in answer]
+answer = random.choice(random_words)
 
 guess = ""
 guesses = 0
-hint = ""
-for char in answer:
-        hint += "_"
+hint = "_" * len(answer)
 
 while guess != answer:
     print("Your hint is:", hint)
+    hint = str.lower(hint)
     guess = input("What is your guess? ")
     if len(guess) == len(answer):
         guesses += 1
         new_hint = ""
         for char in enumerate(guess):
-            if char[1] == answer[char[0]]:
-                new_hint += char[1]
+            if char[1] == answer[char[0]] and char[1] != hint[char[0]]:
+                new_hint += str.upper(char[1])
             else:
                 new_hint += hint[char[0]]
         hint = new_hint
     else:
-        print("Not a valid guess. Please guess again.")
+        print(
+            "Sorry, the guess must have the same number of letters as the secret word."
+        )
 
 print("\nCongratulations! You guessed it!")
 print("It took you {0} guesses.".format(guesses))
